83d6670dcbddba304775a7b8efdf121a36454c6f
Luke Arms <luke@linacreative.com>
2024-08-19 11:34:33 +1000

    Add Xfce4 terminal settings

---
+++ by-platform/linux/Xfce4/files/.config/xfce4/terminal/accels.scm
@@ -0,0 +1,6 @@
+; xfce4-terminal GtkAccelMap rc-file         -*- scheme -*-
+; this file is an automated accelerator map dump
+;
+(gtk_accel_path "<Actions>/terminal-widget/shift-down" "")
+(gtk_accel_path "<Actions>/terminal-widget/shift-up" "")
+(gtk_accel_path "<Actions>/terminal-window/contents" "")
