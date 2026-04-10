3327b41176845c21c27877308f462784be0acd14
Luke Arms <luke@linacreative.com>
2026-04-10 15:27:00 +1000

    Add output size option and set DPI in `magick-prepare-trace`

--- by-default/Bash/files/.bashrc.d/magick.sh
+++ by-default/Bash/files/.bashrc.d/magick.sh
@@ -1,31 +1,28 @@
 #!/usr/bin/env bash
 
-function _magick-get-edge-radius() {
-    local output width height
-    output=$(magick identify -format '%w %h\n' "$in") &&
-        IFS=' ' read -r width height <<<"$output" &&
-        width=$((width > height ? height : width)) &&
-        printf '%d\n' $((width * 625 / 100000))
-}
-
-# magick-prepare-trace [-e|-m] <file> [<output_file> [<edge_radius> [<blend_percent> [<pre_sharpen_radius> [<post_sharpen_radius> [<debug>]]]]]]
+# magick-prepare-trace [-e|-m] [(-h|-w) <size>] <file> [<output_file> [<edge_radius> [<blend_percent> [<pre_sharpen_radius> [<post_sharpen_radius> [<debug>]]]]]]
 #
 # Create a version of <file> where edges are enhanced for hand-tracing.
 #
 # `-compose DivideSrc` is used for edge detection unless -e or -m are given for
 # `-edge` or `-morphology EdgeIn` respectively.
 #
+# If -h or -w are given with <size> (in millimetres), it is used to set output
+# DPI and calculate the default edge radius. No resizing is performed.
+#
 # <file> must exist and have an extension. If <output_file> is not given,
 # "_trace" is inserted before the extension of <file>.
 #
 # Other defaults:
-# - edge_radius: 0.625% of the shortest edge of <file>
-# - blend_percent: 30; increase to 100 for trace image only
+# - edge_radius: 0.7mm if DPI known, otherwise 0.625% of the shortest edge of
+#   <file>
+# - blend_percent: 70; increase to 100 for trace image only, or decrease to see
+#   more of the original image
 # - pre_sharpen_radius: 0; edge_radius * 2.5 if -e is given
 # - post_sharpen_radius: 0
 # - debug: 0
 function magick-prepare-trace() {
-    local edge=0 morphology=0
+    local edge=0 morphology=0 side= size=0
     [[ ${1-} != -e ]] || {
         edge=1
         shift
@@ -34,6 +31,12 @@ function magick-prepare-trace() {
         morphology=1
         edge=0
         shift
+    }
+    [[ ${1-} != @(-h|-w) ]] || {
+        side=${1:1}
+        size=${2:-0}
+        ((size > 0)) || lk_bad_args || return
+        shift 2
     }
 
     [[ -f ${1-} ]] && [[ ${1##*/} == *.* ]] || lk_bad_args || return
@@ -44,9 +47,41 @@ function magick-prepare-trace() {
     ((!$#)) || shift
     [[ $out ]] || out=${in%.*}_trace.${in##*.}
 
+    local args=() args2=()
+
+    local output width height dpi y_dpi p_width p_height
+    output=$(magick identify -units PixelsPerInch -format '%w %h %x %y\n' "$in") &&
+        IFS=' ' read -r width height dpi y_dpi <<<"$output" || return
+
+    ((dpi == y_dpi)) || lk_err "x and y resolutions differ: $in" || return
+
+    ((!size)) || {
+        if [[ $side == w ]]; then
+            dpi=$((width * 254 / size / 10))
+        else
+            dpi=$((height * 254 / size / 10))
+        fi
+        args2+=(
+            -units PixelsPerInch
+            -density $dpi
+        )
+    }
+
+    p_width=$((width * 254 / dpi / 10))
+    p_height=$((height * 254 / dpi / 10))
+
+    ((dpi >= 72)) || lk_tty_warning "DPI lower than 72 for ${p_width}x${p_height}mm"
+
+    # Look for edges in a region roughly 1.4mm across
+    local default_edge_radius
+    default_edge_radius=$(bc -l <<<"$dpi * 7 / 254") &&
+        default_edge_radius=$(printf '%.0f' "$default_edge_radius") &&
+        { ((default_edge_radius)) || default_edge_radius=1; } ||
+        default_edge_radius=$(((width > height ? height : width) * 625 / 100000))
+
     local edge_radius blend_percent pre_sharpen_radius post_sharpen_radius debug
-    edge_radius=${1:-$(_magick-get-edge-radius)} || return
-    blend_percent=${2:-30}
+    edge_radius=${1:-$default_edge_radius}
+    blend_percent=${2:-70}
     if ((edge || morphology)); then
         pre_sharpen_radius=${3:-$((edge_radius * 5 / 2))}
     else
@@ -58,11 +93,11 @@ function magick-prepare-trace() {
     ((debug)) && debug= || unset debug
 
     printf '%s: %s\n' \
-        Input "$in" \
-        Output "$out" \
+        Input "$in (${width}x${height}px at ${y_dpi}DPI)" \
+        Output "$out (${width}x${height}px at ${dpi}DPI; ${p_width}x${p_height}mm)" \
         "Edge radius" "$edge_radius"
 
-    local args=(
+    args+=(
         -colorspace gray
     )
 
@@ -95,5 +130,5 @@ function magick-prepare-trace() {
 
     magick "$in" "${args[@]}" "$out"
     magick composite -blend "$blend_percent" "$out" "$in" "$out"
-    magick "$out" -level "12.5,85%" "$out"
+    magick "$out" -level "12.5,85%" ${args2+"${args2[@]}"} "$out"
 }
