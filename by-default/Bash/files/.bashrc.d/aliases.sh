f03b8a73ccd1667a1749e62c5e17eb50387d5521
Luke Arms <luke@linacreative.com>
2026-05-22 20:34:21 +1000

    Update Nextcloud settings, Brewfiles

--- by-default/Bash/files/.bashrc.d/aliases.sh
+++ by-default/Bash/files/.bashrc.d/aliases.sh
@@ -2,3 +2,4 @@
 
 alias listeners="lsof +c 0 -iTCP -stcp:LISTEN -nP"
 alias remove-icc-profile="exiftool -icc_profile="
+alias start-open-webui="docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main"
