7629b7f5703ff2a320e32ff6aa6a96a37e8717b1
Luke Arms <luke@linacreative.com>
2026-07-03 12:55:05 +1000

    Rework `magick-prepare-trace` options, improve sizing, add grids
    - Adopt `getopts`
    - Replace positional parameters with options where appropriate
    - Rename `-l` to `-a`
    - Add `-l` and `-s` to output size options for "long side" and "short
      side" respectively
    - If multiple size options are given, apply them all
    - Report commands as they are called
    - Add new function `magick-add-grid`

--- by-default/Bash/files/.bashrc.d/aliases.sh
+++ by-default/Bash/files/.bashrc.d/aliases.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 alias listeners="lsof +c 0 -iTCP -stcp:LISTEN -nP"
+alias magick-grid-a4="magick-trace-a4 -o 0 -n"
+alias magick-grid-a4-tight="magick-trace-a4-tight -o 0 -n"
+alias magick-grid-a5="magick-trace-a5 -o 0 -n"
+alias magick-grid-a5-tight="magick-trace-a5-tight -o 0 -n"
+alias magick-trace-a4="magick-prepare-trace -l 240 -s 180 -g15"
+alias magick-trace-a4-tight="magick-prepare-trace -l 284 -s 197 -g15"
+alias magick-trace-a5="magick-prepare-trace -l 160 -s 120 -g15"
+alias magick-trace-a5-tight="magick-prepare-trace -l 197 -s 135 -g15"
 alias remove-icc-profile="exiftool -icc_profile="
 alias start-open-webui="docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main"
--- by-default/Bash/files/.bashrc.d/magick.sh
+++ by-default/Bash/files/.bashrc.d/magick.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+# magick-add-grid <file> [<columns> [<rows> [<width>]]]
+#
+# Add a grid to <file>.
+#
+# If <width> is less than 3 pixels, grid lines are white, otherwise up to 1/3 of
+# each grid line is black and the remainder is white. This improves grid
+# visibility in light-toned parts of <file>.
+function magick-add-grid() {
+    [[ -f ${1-} ]] && [[ ${1##*/} == *.* ]] || lk_bad_args || return
+    local in=$1 columns=$((${2:-3})) rows=$((${3:-3})) width=$((${4:-3}))
+    local shadow_width=$((width / 3))
+    local border_width=$((width - shadow_width))
+
+    local args=(
+        # Divide image into equal tiles
+        -crop "${columns}x${rows}@"
+        # Remove pixels from top-left of each tile
+        -chop "${width}x${width}"
+        # Add pixels with current background colour to top-left of each tile
+        -background black
+        -splice "${shadow_width}x${shadow_width}"
+        -background white
+        -splice "${border_width}x${border_width}"
+        -flatten
+    )
+
+    lk_tty_run_detail magick "$in" "${args[@]}" "$in"
+}
+
 # magick-set-size (-h|-w) <size> <file>
 #
 # Use given height or width (in millimetres) to set the output DPI of <file>
@@ -31,23 +60,54 @@ function magick-set-size() {
         -density $dpi
     )
 
-    magick "$in" "${args[@]}" "$in"
+    lk_tty_run_detail magick "$in" "${args[@]}" "$in"
 }
 
-# magick-prepare-trace [-e|-m] [(-h|-w) <size>] [-l] [-n] <file> [<output_file> [<edge_radius> [<blend_percent> [<pre_sharpen_radius> [<debug>]]]]]
+function _magick-prepare-trace_usage() {
+    cat <<'EOF'
+Usage: magick-prepare-trace [options] <file> [<output_file>]
+
+Create a version of <file> where edges are enhanced for hand-tracing.
+
+<file> must exist and have an extension. If <output_file> is not given,
+"_trace" is inserted before the extension of <file>.
+
+Options:
+
+    -e              Use '-edge' (default: '-compose DivideSrc')
+    -m              Use '-morphology EdgeIn' (default: '-compose DivideSrc')
+    -r <radius>     Set edge radius
+    -p <radius>     Set pre-sharpen radius, or 0 to disable
+    -h <size>       Set maximum height of output when printed (in millimetres)
+    -w <size>       Set maximum width of output when printed (in millimetres)
+    -l <size>       Set maximum length of output's long side when printed (in millimetres)
+    -s <size>       Set maximum length of output's short side when printed (in millimetres)
+    -o <opacity>    Set opacity of trace image when blending with original (0-100, default 70)
+    -a              Use '-auto-level' to normalise edges (default: '-linear-stretch 10x0%')
+    -n              Do not use levels to lighten output for printing
+    -g <size>       Add grid with given spacing (in millimetres)
+
+EOF
+}
+
+# magick-prepare-trace [-e|-m] [-r <edge_radius>] [-p <pre_sharpen_radius>] [(-h|-w|-l|-s) <size>]... [-o <opacity>] [-a] [-n] [-g <size>] [-d] <file> [<output_file>]
 #
 # Create a version of <file> where edges are enhanced for hand-tracing.
 #
 # `-compose DivideSrc` is used for edge detection unless -e or -m are given for
 # `-edge` or `-morphology EdgeIn` respectively.
 #
-# If -h or -w are given with <size> (in millimetres), it is used to set output
-# DPI and calculate the default edge radius. No resizing is performed.
+# If -h (height), -w (width), -l (long side) or -s (short side) are given with a
+# <size> (in millimetres), it is used to set the DPI of the output without
+# changing its pixel count.
 #
-# If -l is given, `-auto-level` is used to normalise edges instead of
+# If -a is given, `-auto-level` is used to normalise edges instead of
 # `-linear-stretch`.
 #
 # If -n is given, levels are not used to lighten output for printing.
+#
+# If -g is given with a <size> (in millimetres), a grid is added to the output
+# with the given spacing between each line.
 #
 # <file> must exist and have an extension. If <output_file> is not given,
 # "_trace" is inserted before the extension of <file>.
@@ -55,30 +115,32 @@ function magick-set-size() {
 # Other defaults:
 # - edge_radius: 0.7mm if DPI known, otherwise 0.625% of the shortest edge of
 #   <file>
-# - blend_percent: 70; increase to 100 for trace image only, or decrease to see
-#   more of the original image
+# - opacity: 70; increase to 100 for trace image only, or decrease to see more
+#   of the original image
 # - pre_sharpen_radius: 0; edge_radius * 2.5 if -e is given
-# - debug: 0
 function magick-prepare-trace() {
-    local IFS=' ' edge=0 morphology=0 side= size=0 normalise="-linear-stretch 10x0%" lighten=1
-    while [[ ${1-} == -* ]]; do
-        case "$1" in
-        -e) edge=1 && morphology=0 ;;
-        -m) morphology=1 && edge=0 ;;
-        @(-h|-w))
-            side=${1:1}
-            size=${2:-0}
-            ((size > 0)) || lk_bad_args || return
-            shift
+    local OPTIND OPTARG opt
+    local IFS=' ' edge=0 morphology=0 edge_radius=-1 pre_sharpen_radius=-1 sizes=() opacity=70 normalise="-linear-stretch 10x0%" lighten=1 grid=-1 debug=0
+    while getopts ":emr:p:h:w:l:s:o:ang:d" opt; do
+        case "$opt" in
+        e) edge=1 && morphology=0 ;;
+        m) morphology=1 && edge=0 ;;
+        r) edge_radius=$((OPTARG)) ;;
+        p) pre_sharpen_radius=$((OPTARG)) ;;
+        h | w | l | s)
+            ((OPTARG > 0)) || lk_usage || return
+            sizes+=("$opt" "$((OPTARG))")
             ;;
-        -l) normalise=-auto-level ;;
-        -n) lighten=0 ;;
-        *) lk_warn "invalid option: $1" || return ;;
+        o) opacity=$((OPTARG)) ;;
+        a) normalise=-auto-level ;;
+        n) lighten=0 ;;
+        g) grid=$((OPTARG)) ;;
+        d) debug=1 ;;
         esac
-        shift
     done
+    shift $((OPTIND - 1))
 
-    [[ -f ${1-} ]] && [[ ${1##*/} == *.* ]] || lk_bad_args || return
+    [[ -f ${1-} ]] && [[ ${1##*/} == *.* ]] || lk_usage || return
     local in=$1
     shift
 
@@ -88,23 +150,31 @@ function magick-prepare-trace() {
 
     local args=() args2=()
 
-    local output width height dpi y_dpi p_width p_height
+    local output width height dpi y_dpi short long i px size_dpi p_width p_height
     output=$(magick identify -units PixelsPerInch -format '%w %h %x %y\n' "$in") &&
         read -r width height dpi y_dpi <<<"$output" || return
 
     ((dpi == y_dpi)) || lk_err "x and y resolutions differ: $in" || return
 
-    ((!size)) || {
-        if [[ $side == w ]]; then
-            dpi=$((width * 254 / size / 10))
-        else
-            dpi=$((height * 254 / size / 10))
-        fi
-        args2+=(
-            -units PixelsPerInch
-            -density $dpi
-        )
-    }
+    long=$((width > height ? width : height))
+    short=$((width > height ? height : width))
+
+    # Adopt the size with the highest DPI (smallest size)
+    for ((i = 0; i < ${#sizes[@]}; i += 2)); do
+        case "${sizes[i]}" in
+        h) px=$height ;;
+        w) px=$width ;;
+        l) px=$long ;;
+        s) px=$short ;;
+        esac
+        size_dpi=$((px * 254 / ${sizes[i + 1]} / 10))
+        dpi=$((!i || size_dpi > dpi ? size_dpi : dpi))
+    done
+    args2+=(
+        -units PixelsPerInch
+        -density $dpi
+    )
+
     ((!lighten)) ||
         args2+=(
             -level "12.5,85%"
@@ -115,29 +185,30 @@ function magick-prepare-trace() {
 
     ((dpi >= 72)) || lk_tty_warning "DPI lower than 72 for ${p_width}x${p_height}mm"
 
-    # Look for edges in a region roughly 1.4mm across
-    local default_edge_radius
-    default_edge_radius=$(bc -l <<<"$dpi * 7 / 254") &&
-        default_edge_radius=$(printf '%.0f' "$default_edge_radius") &&
-        { ((default_edge_radius)) || default_edge_radius=1; } ||
-        default_edge_radius=$(((width > height ? height : width) * 625 / 100000))
+    ((edge_radius > -1)) || {
+        # Look for edges in a region roughly 1.4mm across
+        edge_radius=$(bc -l <<<"$dpi * 7 / 254") &&
+            edge_radius=$(printf '%.0f' "$edge_radius") &&
+            { ((edge_radius)) || edge_radius=1; } ||
+            edge_radius=$((short * 625 / 100000))
+    }
 
-    local edge_radius blend_percent pre_sharpen_radius debug
-    edge_radius=${1:-$default_edge_radius}
-    blend_percent=${2:-70}
-    if ((edge || morphology)); then
-        pre_sharpen_radius=${3:-$((edge_radius * 5 / 2))}
-    else
-        pre_sharpen_radius=${3:-0}
-    fi
-    debug=${4:-0}
+    ((pre_sharpen_radius > -1)) ||
+        if ((edge || morphology)); then
+            pre_sharpen_radius=$((edge_radius * 5 / 2))
+        else
+            pre_sharpen_radius=0
+        fi
 
     ((debug)) && debug= || unset debug
 
-    printf '%s: %s\n' \
+    printf '%s\t%s\n' \
         Input "$in (${width}x${height}px at ${y_dpi}DPI)" \
         Output "$out (${width}x${height}px at ${dpi}DPI; ${p_width}x${p_height}mm)" \
-        "Edge radius" "$edge_radius"
+        "Edge radius" "$edge_radius" \
+        "Pre-sharpen radius" "$pre_sharpen_radius" \
+        Method "$(if ((morphology)); then echo "morphology (EdgeIn with diamond kernel)"; elif ((edge)); then echo "edge detection"; else echo DivideSrc; fi)" |
+        lk_tty_detail_pairs
 
     # Remove debug output from previous run
     rm -f \
@@ -176,7 +247,12 @@ function magick-prepare-trace() {
         ${debug+-write "${out%.*}_03_edge.${out##*.}"}
     )
 
-    magick "$in" "${args[@]}" "$out"
-    magick composite -blend "$blend_percent" "$out" "$in" "$out"
-    magick "$out" ${args2+"${args2[@]}"} "$out"
+    lk_tty_run_detail magick "$in" "${args[@]}" "$out" || return
+    ((opacity == 100)) || lk_tty_run_detail magick composite -blend "$opacity" "$out" "$in" "$out" || return
+    lk_tty_run_detail magick "$out" ${args2+"${args2[@]}"} "$out" || return
+    ((grid < 1)) || {
+        local grid_width=$(((edge_radius + 1) / 2))
+        magick-add-grid "$out" $((p_width / grid)) $((p_height / grid)) $((grid_width + (3 - grid_width % 3) % 3))
+    }
+    lk_tty_success "Ready to print:" "$(realpath "$out")"
 }
